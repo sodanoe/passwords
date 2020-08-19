4f288f846e220369e6db6ae80335df1f763efbff
sodanoe <sodanoe@gmail.com>
2020-08-19 19:59:27 +0300

    start to deal with working part

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"Users/sergeynogin/DEV/golang/passwords/menu"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
@@ -9,7 +8,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	// "os"
+	"os"
 )
 
 //Encrypt - шифрование
@@ -61,57 +60,101 @@ func GenerateKey() ([]byte, error) {
 func main() {
 
 	//File openings
+	cypher := "cypher"
+	wad := "data"
 
-	// kkey := []byte("le'gj[rjv")
+	cypherFile, err := os.OpenFile(cypher, os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cypherFile.Close()
 
-	// cypher := "cypher.txt"
-	// shit := "shit.txt"
-
-	// cypherFile, err := os.OpenFile(cypher, os.O_RDWR, 0644)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer cypherFile.Close()
-
-	// shitFile, err := os.OpenFile(shit, os.O_RDWR, 0644)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer shitFile.Close()
-
-	//
+	dataFile, err := os.OpenFile(wad, os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dataFile.Close()
 
 	data := []byte("our super secret text")
 
+	// Инициализация среза для хранения данных в памяти
+
+	//var allData []string
+	allData := make([]string, 1)
+	allData[0] = "user:password"
+	fmt.Println(allData)
+
+	var i int
+	for i != 4 {
+		fmt.Println("Меню: ")
+		fmt.Println("1. Вывод данных")
+		fmt.Println("2. Пополнить таблицу")
+		fmt.Println("3. Вывести какой-то хуйни")
+		fmt.Println("4. Выход")
+
+		fmt.Scan(&i)
+		switch i {
+		case 1:
+			fmt.Println(allData)
+		case 2:
+			{
+				var name string
+				var password string
+				for {
+					fmt.Println("Please input username: ")
+					fmt.Scan(&name)
+					if name == "stop" {
+						break
+					}
+
+					fmt.Println("Please input password: ")
+					fmt.Scan(&password)
+
+					result := name + ":" + password
+					allData = append(allData, result)
+				}
+
+				fmt.Println(allData)
+			}
+		case 3:
+			{
+				kkey, err := ioutil.ReadFile("cypher")
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if err != nil {
+					log.Fatal(err)
+				}
+				ciphertext, err := Encrypt(kkey, data)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf("ciphertext: %s\n", hex.EncodeToString(ciphertext))
+				plaintext, err := Decrypt(kkey, ciphertext)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf("plaintext: %s\n", plaintext)
+			}
+		case 4:
+			break
+		}
+	}
+	// здесь ФУНКЦИЯ для ввода данных в память
+
 	// key, err := GenerateKey()
-
-	//Write key to cypher.txt file
-
+	// Write key to cypher file
 	// err = ioutil.WriteFile(cypher, key, 0644)
 	// if err != nil {
 	//     return
 	// }
-	//
-	//
 
-	menu.PrintShit()
+	// Схемка для хранения:
 
-	kkey, err := ioutil.ReadFile("cypher.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// цикл запроса ввода от пользователя вида:
+	// 1. Вывод ЮЗЕРОВ для выбора вывода их пароля
+	// 2. изменение списка
+	// 3. выход (и последующая запись с шифрованием)
 
-	if err != nil {
-		log.Fatal(err)
-	}
-	ciphertext, err := Encrypt(kkey, data)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("ciphertext: %s\n", hex.EncodeToString(ciphertext))
-	plaintext, err := Decrypt(kkey, ciphertext)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("plaintext: %s\n", plaintext)
 }
